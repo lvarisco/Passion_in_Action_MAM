e36a1c6b01abc561895316e9db901c69a3b78719
lvarisco <74761622+lvarisco@users.noreply.github.com>
2022-10-06 17:26:12 +0200

    Update Lez3.m

--- Lezione 3/Lez3.m
+++ Lezione 3/Lez3.m
@@ -1,25 +1,27 @@
 clear
 close all
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% esercizio Asta appoggiata
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%costanti
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Esercizio asta appoggiata
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Costanti
 alfa=0;
 beta=(180-45)/180*pi; % [rad]
 c=0.7;% [m]
-%%%input atto di moto considerato
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input atto di moto considerato
 gamma=(180-30)/180*pi;
 vA=5; % [m/s]
 aA=0.3; % [m/s^2]
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%posizione
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Posizione
 x0=[c 0];
 F=@(x) [x(1)*cos(alfa)+c*cos(gamma)-x(2)*cos(beta);
         x(1)*sin(alfa)+c*sin(gamma)-x(2)*sin(beta)];
 sol=fsolve(F,x0);
 a=sol(1);
 b=sol(2);
-%%velocità
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Velocità
 ap=vA;
 AA=[cos(beta) c*sin(gamma);
     sin(beta) -c*cos(gamma)];
@@ -31,7 +33,8 @@ gammap=xp(2);
 
 disp(['La velocità del punto B vale: ' num2str(bp) 'm/s']);
 disp(['La velocità angolare dell''asta vale: ' num2str(gammap) 'rad/s']);
-%%accelerazione
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Accelerazione
 app=aA;
 AA=[cos(beta) c*sin(gamma);
     sin(beta) -c*cos(gamma)];
@@ -44,20 +47,23 @@ gammapp=xpp(2);
 disp(['L''accelerazione del punto B vale: ' num2str(bpp) 'm/s^2']);
 disp(['L''accelerazione angolare dell'' asta vale: ' num2str(gammapp) 'rad/s^2']);
 
-%%legge di moto (acc di A costante)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Legge di moto (acc di A costante)
 dt=0.001; %s
 tmax=0.08;
 tempo=0:dt:tmax;
 vAtime=vA+aA*tempo;
 sAtime=a+vA*tempo+0.5*aA*tempo.^2; %N.B.: il tempo è un vettore
-%%%inizializzo variabili
+
+% Inizializzo variabili
 btime=zeros(size(tempo));
 gammatime=zeros(size(tempo));
 bptime=zeros(size(tempo));
 gammapptime=zeros(size(tempo));
 bpptime=zeros(size(tempo));
 gammapptime=zeros(size(tempo));
-%%%assegno istante t=0
+
+% Aassegno istante t=0
 btime(1)=b;
 gammatime(1)=gamma;
 bptime(1)=bp;
